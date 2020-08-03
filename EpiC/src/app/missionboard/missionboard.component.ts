d7cc9eabd747d85ba7aea7c758628a36510eade2
rom <romcohen115@gmail.com>
2020-08-03 12:48:33 +0300

    cr

--- EpiC/src/app/missionboard/missionboard.component.ts
+++ EpiC/src/app/missionboard/missionboard.component.ts
@@ -1,16 +1,69 @@
 import { Component, OnInit } from '@angular/core';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
+interface statusObj{
+  statusName:string,
+  statusList:string[]
+}
+
 @Component({
   selector: 'app-missionboard',
   templateUrl: './missionboard.component.html',
   styleUrls: ['./missionboard.component.css']
 })
+
 export class MissionboardComponent implements OnInit {
 
   teamName="צוות מנצח";
 
   constructor() { }
+  // statusArray:statusObj[]=[
+  //   {
+  //     statusName:'כל המשימות',
+  //     statusList:[
+  //     'לחקור את רום',
+  //     'לחקור את אלאור',
+  //     ]
+  //   },
+  //   {
+  //     statusName:'בתהליך',
+  //     statusList: [
+  //       'לחקור את רום',
+  //       'לחקור את אלאור',
+  //     ],
+  //   },
+  //   {
+  //     statusName:'נחקר וטרם הוקלד',
+  //     statusList:[
+
+  //     ],
+  //   },
+  //   {
+  //     statusName:'הוקלד',
+  //     statusList:[
+  //       'לחקור את דנה',
+  //       'לחקור את אביב'
+  //     ],
+  //   },
+  //   {
+  //     statusName:'פיידבק',
+  //     statusList: [
+  //       'לחקור את טל',
+  //       'לחקור את צחי',
+  //       'לחקור את דנה',
+  //       'לחקור את אביב'
+  //     ],
+  //   },
+  //   {
+  //     statusName:'הסתיים',
+  //     statusList:[
+  //       'לחקור את דנה',
+  //       'לחקור את אביב'
+  //     ]
+  //   }
+  // ];
+
+
 
   alltasks = [
     'לחקור את רום',
@@ -46,12 +99,13 @@ export class MissionboardComponent implements OnInit {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
+      console.log(event.container.data)
+      console.log(event.previousContainer.data)
       transferArrayItem(event.previousContainer.data,
                         event.container.data,
                         event.previousIndex,
                         event.currentIndex);
     }
-    console.log(this.alltasks)
   }
 
   ngOnInit() {
--- EpiC/src/app/sidenav/sidenav.component.ts
+++ EpiC/src/app/sidenav/sidenav.component.ts
@@ -1,51 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NestedTreeControl } from '@angular/cdk/tree';
 import { MatTreeNestedDataSource } from '@angular/material/tree';
-import {  } from '@angular/material/button'
+import { side_nav_data, sectionNode } from './tree-data-config';
 
-interface sectionNode {
-  name: string;
-  children?: sectionNode[];
-}
-const TREE_DATA: sectionNode[] = [
-  {
-    name: 'צפון',
-    children: [
-      {name: 'צפת'},
-      {name: 'כנרת'},
-    ]
-  },
-  {
-    name: 'חיפה',
-    children: [
-      {name: 'חיפה'},
-      {name: 'חדרה'},
-    ]
-  },
-  {
-    name: 'מרכז',
-    children: [
-      {name: 'שרון',
-      children:[
-        {name:'צוות חקירה אלוף'},
-        {name:'צוות חקירה מנצח'},
-        {name:'צוות חקירה ענק'},
-      ]
-    },
-      {name: 'רמלה'},
-      {name: 'רחובות'},
-    ]
-  },
-  {
-    name: 'ירושלים',
-  },
-  {
-    name: 'אשקלון',
-  },
-  {
-    name: 'דרום',
-  },
-];
 
 @Component({
   selector: 'app-sidenav',
@@ -56,12 +13,12 @@ export class SidenavComponent implements OnInit {
 
   treeControl = new NestedTreeControl<sectionNode>(node => node.children);
   dataSource = new MatTreeNestedDataSource<sectionNode>();
-  message='תמונת מצב מחוזות'
-  text='סהכ חקירות פתוחות : '
-  openInvs=1000;
+  message = side_nav_data.message
+  text = side_nav_data.text
+  openInvs = 1000
 
   constructor() {
-    this.dataSource.data = TREE_DATA;
+    this.dataSource.data = side_nav_data.TREE_DATA;
    }
 
   ngOnInit() {
